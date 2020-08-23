563c88c6a5ccfa162c488949f2ccad4569ecbf30
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-23 09:07:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ function log(message) {
   logArea.innerHTML = logArea.innerHTML + message;
 }
 log('Something');
-
+const buf = new ArrayBuffer(8)
+const view = new DataView(buf)
+[64, -124, 12, 0, 0, 0, 0, 0].forEach((b, i) => view.setUint8(i, b))
+log(view.getFloat64(0))
 alert('ok')
