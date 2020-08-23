fc62f0828df302fc8169dfab384d8324bc9e3d38
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-23 09:26:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-var text = '';
- 
 $.ajax({
-	url: 'https://google.com',
+	url: '/index.php',
 	method: 'get',
-	dataType: 'html',
-	async: false,
+	dataType: 'json',
 	success: function(data){
-		text = data;
+		alert(data);
+	},
+	error: function (jqXHR, exception) {
+		if (jqXHR.status === 0) {
+			alert('Not connect. Verify Network.');
+		} else if (jqXHR.status == 404) {
+			alert('Requested page not found (404).');
+		} else if (jqXHR.status == 500) {
+			alert('Internal Server Error (500).');
+		} else if (exception === 'parsererror') {
+			alert('Requested JSON parse failed.');
+		} else if (exception === 'timeout') {
+			alert('Time out error.');
+		} else if (exception === 'abort') {
+			alert('Ajax request aborted.');
+		} else {
+			alert('Uncaught Error. ' + jqXHR.responseText);
+		}
 	}
 });
- 
-alert(text);
-alert('df')
