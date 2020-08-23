103c922e8493cab1bc28e10666aac4f86d7d912f
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-23 09:20:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function log(message) {
-  let logArea = document.getElementById('log');
-  logArea.innerHTML = logArea.innerHTML + message;
-}
-log('Something');
-const buf = new ArrayBuffer(8)
-const view = new DataView(buf)
-[64, -124, 12, 0, 0, 0, 0, 0].forEach((b, i) => view.setUint8(i, b))
-log(view.getFloat64(0))
+$("#form").on("submit", function(){
+	$.ajax({
+		url: '/handler.php',
+		method: 'post',
+		dataType: 'html',
+		data: $(this).serialize(),
+		success: function(data){
+			$('#message').html(data);
+		}
+	});
+});
 alert('ok')
