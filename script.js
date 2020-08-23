79d9c5cb632276dc212eb5f043b7689370b60f3d
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-23 09:52:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 let xhr = new XHR();
 xhr.open('GET', 'https://google.com', true); 
 xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-xhr.setRequestHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-xhr.setRequestHeader('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type');
-xhr.setRequestHeader('Access-Control-Max-Age', '86400');
 xhr.onload = function() {alert( 'Успешно! '); }
 xhr.onerror = function() { alert( `Ошибка ${xhr.status}: ${xhr.statusText}` ); } 
 xhr.send();
