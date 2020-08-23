443a4fd5754734ec24026439ba3d9970811daa18
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-23 21:53:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest; 
 let xhr = new XHR();
-function send(value){
+function send(){
   xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://google.com, true); 
 
 xhr.onload = function() {document.write(xhr.response) }
