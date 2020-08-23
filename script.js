c74b11499654c1d0626659f8c80dad3a54c4bcd5
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-23 10:12:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest; 
 let xhr = new XHR();
 xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-xhr.open('GET', 'https://google.com', true); 
+xhr.open('GET', 'https://cors.io/?https://google.com', true); 
 xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
 xhr.onload = function() {alert( 'Успешно! '); }
 xhr.onerror = function() { alert( `Ошибка ${xhr.status}: ${xhr.statusText}` ); } 
