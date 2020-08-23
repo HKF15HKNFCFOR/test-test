508686461192ab8011f02fe7f0be4a4647189d43
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-23 10:41:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest; 
 let xhr = new XHR();
 
-xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://google.com', true); 
+xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://films2020.club/', true); 
 
 xhr.onload = function() {alert( 'Успешно! '); }
 xhr.onerror = function() { alert( `Ошибка ${xhr.status}: ${xhr.statusText}` ); } 
