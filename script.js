48b36d20c226cde7d78487232a9e35a106b5731c
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-24 12:27:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-alert('ko')
-function sendok(){
-  alert('ok')
-  let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest; 
-let xhr = new XHR();
-  xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://google.com, true); 
-
-xhr.onload = function() {document.write(xhr.response) }
-xhr.onerror = function() { alert( `Ошибка ${xhr.status}: ${xhr.statusText}` ) } 
-xhr.send();
-}
+alert("ghh");
