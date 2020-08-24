72622fcec975b9a6cfc0a536c80bc48fffc1cb5b
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-24 16:35:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 let xhr = new XHR();
 let hyptext=[]
 
-function sendok(value){
+function sendok(id){
+  value=document.getElementById('textbox'+id).value
 if(value =='' || value==' '){
 alert('Введите url')
 } else {
 xhr.open('GET', 'https://cors-anywhere.herokuapp.com/http://www.'+value, true); 
-xhr.onprogress = function() { document.getElementById('textout1').innerHTML="Статус: загрузка..." }
+xhr.onprogress = function() { document.getElementById('textout'+id).innerHTML="Статус: загрузка..." }
 xhr.onload = function() {
 hyptext.append(xhr.response)
-document.getElementById('textout1').innerHTML="Статус: загружено!"
-document.getElementById('textout1').innerHTML+='<input type="button" value="Перейти!" onclick="opend(hyptext[0])" />'
+document.getElementById('textout'+id).innerHTML="Статус: загружено!"
+document.getElementById('textout'+id).innerHTML+='<input type="button" value="Перейти!" onclick="opend(hyptext['+id+'])" />'
 }
 xhr.onerror = function() { alert( `Ошибка ${xhr.status}: ${xhr.statusText}` ) } 
 xhr.send();
