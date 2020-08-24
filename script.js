4cbf3b25ce6e9093f1679397140d392bf951752c
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-24 12:48:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function sendok(){
- let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest; 
+let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest; 
 let xhr = new XHR();
+let hyptext=''
+
+function sendok(){
   xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://google.com', true); 
 xhr.onprogress = function(event) {
   document.getElementById('but').value="Загрузка..."
 }
 xhr.onload = function() {
+ hyptext= xhr.response
  document.getElementById('but').value="Загружено!"
-document.getElementById('butpos').innerHTML='<input type="button" value="Перейти!" onclick="document.write(xhr.response)" />'
+document.getElementById('butpos').innerHTML='<input type="button" value="Перейти!" onclick="document.write(hyptext)" />'
   }
 xhr.onerror = function() { alert( `Ошибка ${xhr.status}: ${xhr.statusText}` ) } 
 xhr.send();
