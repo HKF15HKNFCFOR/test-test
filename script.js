cc6c0039a01091b867aa0dfb4479db97e39f19f8
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-24 16:50:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ alert('Введите url')
 let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest; 
 let xhr = new XHR();
 xhr.open('GET', 'https://cors-anywhere.herokuapp.com/http://www.'+value, true); 
- alert('https://cors-anywhere.herokuapp.com/http://www.'+value)
 xhr.onprogress = function() { document.getElementById('textout'+id).innerHTML="Статус: загрузка..." }
 xhr.onload = function() {
 hyptext.push(xhr.response)
